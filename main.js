33458874c00febe2e1b7dbd714d0efee15ae63d8
lucas_inestal <lucasainestal@gmail.com>
2019-09-27 10:34:08 +0200

    :
    changes with file structure:

---
+++ main.js
@@ -0,0 +1,56 @@
+(function () {
+
+const question1 = new Question(
+    "Geografi",
+    "Vilket är världens högsta berg?",
+    "Mount Everest",
+    [
+        "Mount Everest",
+        "K2",
+        "Kilamanjaro"
+    ]
+);
+const question2 = new Question(
+    "Biologi",
+    "Vad är betecking för guld?",
+    "AG",
+    [
+        "ZE",
+        "AG",
+        "kY"
+    ]
+);
+const question3 = new Question(
+    "Biologi2",
+    "Vad är betecking för guld?",
+    "AG",
+    [
+        "ZE",
+        "AG",
+        "kY"
+    ]
+);
+const question4 = new Question(
+    "Biologi3",
+    "Vad är betecking för guld?",
+    "AG",
+    [
+        "ZE",
+        "AG",
+        "kY"
+    ]
+);
+
+const quiz = new Quiz([
+    question1,
+    question2,
+    question3,
+    question4
+]);
+//quiz.loadJsonQuestions(json)
+quiz.render();
+
+let x = getJSON('https://www.mocky.io/v2/5d8dc28b310000cf032b5016')
+console.log(x);
+
+})();
